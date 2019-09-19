2f005cdaf906b7fbb222677ef491938278b3c6d8
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-19 09:16:11 -0700

    Add Rover instantiation of dog class

--- dog.py
+++ dog.py
@@ -1,3 +1,11 @@
-class Dog(self, name):
+class Dog:
     def __init__(self, name):
         self.name = name
+
+
+#this ends the Dog class
+
+
+my_dog = Dog("Rover")
+print(my_dog)
+print(my_dog.name)
