db97dfc415305c0d37f98bca02c87e43ec82c3ba
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-19 09:38:00 -0700

    add property of breed

--- dog.py
+++ dog.py
@@ -1,11 +1,13 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
 
 
 #this ends the Dog class
 
 
-my_dog = Dog("Rover")
+my_dog = Dog("Rover", "SuperDog")
 print(my_dog)
 print(my_dog.name)
+print(my_dog.breed)
