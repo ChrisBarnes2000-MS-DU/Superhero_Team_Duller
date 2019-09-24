351d9f587788e8dfbf9dd9af9cb6919e6e15fbee
Christopher Barnes <cbarnes7404@yahoo.com>
2019-09-23 22:56:58 -0700

    add code to randomize attack ability

--- superheros.py
+++ superheros.py
@@ -1,10 +1,17 @@
+import random
+
 class Ability():
     def __init__(self, name, max_damage):
+       '''Create Instance Variables:
+          name:String
+          max_damage: Integer
+       '''
        self.name = name
        self.max_damage = max_damage
 
     def attack(self):
-        pass
+        ''' Return a value between 0 and the value set by self.max_damage.'''
+        return random.randint(0, self.max_damage)
 
 class Armor():
     def __init__(self, name, max_block):
@@ -36,3 +43,11 @@ class Hero():
 
     def fight(Hero):
         pass
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
